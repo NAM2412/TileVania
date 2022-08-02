cdddfb1c9ddab0ae20b6259af976227c360822fd
nguyennamhades <nnhnam2345@gmail.com>
2022-08-02 14:48:12 +0700

    set Running state for Umi

--- Assets/Scripts/PLayerMovement.cs
+++ Assets/Scripts/PLayerMovement.cs
@@ -7,10 +7,13 @@ public class PLayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
     Vector2 moveInput;
-    Rigidbody2D Myrigidbody;
+    Rigidbody2D myRigidbody;
+    
+    Animator myAnimator;
     void Start()
     {
-        Myrigidbody = GetComponent<Rigidbody2D>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
 
@@ -22,18 +25,21 @@ public class PLayerMovement : MonoBehaviour
 
     private void FlipSprite()
     {
-        bool IsPlayerHasHorizontalSpeed = Mathf.Abs(Myrigidbody.velocity.x) > Mathf.Epsilon; // so sánh xem nhân vật có đang di chuyển hay không`
+        bool IsPlayerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; // so sánh xem nhân vật có đang di chuyển hay không`
 
         if (IsPlayerHasHorizontalSpeed)
         {
-            transform.localScale = new Vector2 (Mathf.Sign(Myrigidbody.velocity.x),1f);
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x),1f);
         }
     }
 
     private void Run()
     {
-            Vector2 playerVelocity = new Vector2(moveInput.x*runSpeed, Myrigidbody.velocity.y);
-            Myrigidbody.velocity = playerVelocity;
+            Vector2 playerVelocity = new Vector2(moveInput.x*runSpeed, myRigidbody.velocity.y);
+            myRigidbody.velocity = playerVelocity;
+
+            bool IsPlayerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon; // so sánh xem nhân vật có đang di chuyển hay không`
+            myAnimator.SetBool("IsRunning",IsPlayerHasHorizontalSpeed);
     }
 
     private void OnMove(InputValue value)
