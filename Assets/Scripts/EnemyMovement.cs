9b981d9d9a26e381f477c989c1d03049fa001de0
nguyennamhades <nnhnam2345@gmail.com>
2022-08-09 11:34:10 +0700

    Setting DooDoo movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float EmoveSpeed = 1f;
+    Rigidbody2D Erigibody;
+    BoxCollider2D EboxCollider;
+    SpriteRenderer Esprite;
+    void Start()
+    {
+       Erigibody = GetComponent<Rigidbody2D>();
+       EboxCollider = GetComponent<BoxCollider2D>();
+       Esprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Erigibody.velocity = new Vector2(EmoveSpeed,Erigibody.velocity.y);
+    }
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        EmoveSpeed = -EmoveSpeed;
+        FlipEnemyFacing();
+    }
+
+    private void FlipEnemyFacing()
+    {
+         transform.localScale = new Vector2 (-(Mathf.Sign(Erigibody.velocity.x)),1f);
+    }
+}
